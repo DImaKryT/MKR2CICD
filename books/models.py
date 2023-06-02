1f0b20fc484f11b2f3d3c4b1e85f8ec0543342c0
Dim0_0n <37013221+DImaKryT@users.noreply.github.com>
2023-06-02 13:03:32 +0300

    Update models.py

--- books/models.py
+++ books/models.py
@@ -19,3 +19,11 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Recipe(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    year = models.IntegerField()
+
+    def __str__(self):
+        return self.title
