dd2772b7081aa6a6a88d46029842a7eb8918139e
Dim0_0n <37013221+DImaKryT@users.noreply.github.com>
2023-06-02 12:28:22 +0300

    Update views.py

--- books/views.py
+++ books/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def demoPage(request):
+    return HttpResponse("main Page")
+
+def demoPageTemplate(request):
+    return render(request,"main.html")
